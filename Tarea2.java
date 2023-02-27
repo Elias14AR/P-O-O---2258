c2134b3ba8aac005850dfe695bc54f18f2ded558
Elias14AR <119818156+Elias14AR@users.noreply.github.com>
2023-02-26 23:28:44 -0600

    Tarea 2

---
+++ Tarea2.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+public class Tarea2 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        boolean suces = true ;
+        String input;
+
+        System.out.println("Ingresa una sucesion: ");
+        input = teclado.nextLine();
+        System.out.println("Tecleaste " + input);
+
+            for (int i=0; i<input.length()-1;i++){
+                if ((int) input.charAt(i+1) - (int)input.charAt(i)!=1) {
+                    suces = false;
+                }
+        }
+        System.out.println("La sucesion es : " + suces);
+
+    }
+}
