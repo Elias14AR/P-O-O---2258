1e9427447a440c39ab1ab0d88078340bcc1e0f31
Elias14AR <119818156+Elias14AR@users.noreply.github.com>
2023-05-02 19:23:42 -0600

    Add files via upload

---
+++ Tarea 9/Main.java
@@ -0,0 +1,14 @@
+package Tarea9.poo;
+
+import Tarea9.poo.iu.Principal;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Principal ventana = new Principal("Hola mundo AWT");
+
+
+    }
+
+}
